cbc65bf1291faeacf69315c834d3b5959d11e1ce
LordCobar <cobar1337@gmail.com>
2013-05-14 09:43:13 -0500

    ProblemSet2

--- ProblemSet2/bin/ProblemSet2/core_test.clj
+++ ProblemSet2/bin/ProblemSet2/core_test.clj
@@ -3,5 +3,12 @@
         ProblemSet2.core))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Hist_Test"
+    (is (= {}
+       (build-histogram "")))
+(is (= {\a 1, \b 1, \c 1})
+    (build-histogram "cba"))
+(is (= {\x 3})
+    (build-histogram "xxx"))
+(is (= {\space 3, \a 1, \c 2, \e 3, \h 1, \i 3, \, 2, \M 3, \N 1, \n 2, \o 2, \P 1, \r 2, \s 2, \t 1} 
+       (build-histogram "Nic McPhee, Morris, Minnesota")))
--- ProblemSet2/src/ProblemSet2/Histogram.clj
+++ ProblemSet2/src/ProblemSet2/Histogram.clj
@@ -1,3 +1,15 @@
+(ns ProblemSet2.Histogram)
+(use 'clojure.test)
+
 (defn histogram [coll]
    (let [gp (group-by identity coll)] 
       (zipmap (keys gp) (map #(count (second %)) gp))))
+
+(is (= {}
+       (histogram "")))
+(is (= {\a 1, \b 1, \c 1})
+    (histogram "cba"))
+(is (= {\x 3})
+    (histogram "xxx"))
+(is (= {\space 3, \a 1, \c 2, \e 3, \h 1, \i 3, \, 2, \M 3, \N 1, \n 2, \o 2, \P 1, \r 2, \s 2, \t 1} 
+       (histogram "Nic McPhee, Morris, Minnesota")))
